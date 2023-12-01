644671f16fd1b080220dd70ad1b6640fadb96ffe
maestro <polishuk1003@gmail.com>
2023-12-01 22:29:25 +0700

    add iframe

---
+++ src/app/components/editor/editor.component.ts
@@ -0,0 +1,55 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DomSanitizer} from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-editor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './editor.component.html',
+  styleUrl: './editor.component.scss'
+})
+export class EditorComponent implements OnInit, AfterViewInit {
+  iframeSrc: string;
+  @ViewChild('iframe', {static: true}) iframe: ElementRef<HTMLIFrameElement>;
+
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.iframeSrc = 'assets/project/index.html';
+  }
+
+  ngOnInit(): void {
+    this.iframe.nativeElement.src = this.iframeSrc;
+  }
+
+  ngAfterViewInit(): void {
+    if (this.iframe.nativeElement.contentDocument) {
+      setTimeout(() => {
+
+        let body = this.iframe.nativeElement.contentDocument.body;
+        let textNodes = [];
+
+        function recursy(element) {
+          element.childNodes.forEach(node => {
+
+            if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, "").length > 0) {
+              textNodes.push(node);
+            } else {
+              recursy(node);
+            }
+          })
+        }
+
+        recursy(body);
+
+        textNodes.forEach(node => {
+          const wrapper = this.iframe.nativeElement.contentDocument.createElement('text-editor');
+          node.parentNode.replaceChild(wrapper, node);
+          wrapper.appendChild(node);
+          wrapper.contentEditable = "true";
+        });
+        console.log(body.innerHTML, this.iframe.nativeElement.contentDocument.body, textNodes);
+      }, 1000);
+    }
+  }
+}
